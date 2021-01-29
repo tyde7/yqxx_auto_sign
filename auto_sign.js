96ec3dfd98678f1d6f3eca9757f0466d2381fd2a
tyde7 <32335591+tyde7@users.noreply.github.com>
2021-01-29 11:07:17 +0800

    [bugfix] Provide geolocation simulation.

--- auto_sign.js
+++ auto_sign.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const os = require('os');
 const process = require('process');
 const { exception } = require("console");
+const { parse } = require("path");
 is_same_day = function (t) {
   return new Date(t).toDateString() === new Date().toDateString();
 };
@@ -12,8 +13,8 @@ const passwords = [
   {
     u: process.env.JWC_USERNAME,
     p: process.env.JWC_PASSWORD,
-    lat: process.env.SIM_LAT,
-    long: process.env.SIM_LONG,
+    lat: parseFloat(process.env.SIM_LAT),
+    long: parseFloat(process.env.SIM_LONG),
     sa: process.env.WECHAT_ROBOT_HOOK,
     current_pos: process.env.CURRENT_POS
   }
@@ -44,17 +45,31 @@ const passwords = [
         break
     }
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: Boolean(process.env.CI),
       executablePath: platformExecutablePath,
       args: args
     });
+
+
     const page = await browser.newPage();
+    await page.setRequestInterception(true);
+    page.on('request', async (request) => {
+      // 如果文件类型为image,则中断加载
+      if (request.resourceType() === 'image') {
+        request.abort();
+        return;
+      }
+      // 正常加载其他类型的文件
+      request.continue();
+    });
     await page.goto('http://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/yqxx');
     await page.type("#username", a.u, { delay: 100 })
     await page.type("#password", a.p, { delay: 100 })
     await page.click('#casLoginForm > p:nth-child(5) > button')
     await page.waitFor(5000)
 
+
+    console.log("start")
     await page.goto('http://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xs/yqxx');
     let try_time = 5;
     let latest_upload = "";
@@ -67,6 +82,7 @@ const passwords = [
         break;
       } catch {
         try_time -= 1;
+        console.log(`Trytime decreased once. Now is ${try_time}.`)
       }
     }
     if (try_time == 0) {
@@ -74,13 +90,28 @@ const passwords = [
       console.log("Found error. Try_time is zero but iframe is still loading. Critial internet issue?")
       return;
     }
-    if (is_same_day(latest_upload)) {
+    let resubmit = false;
+    status = await page.evaluate(() => {
+      return document.getElementsByClassName("content2")[0].getElementsByClassName("content_title")[1].innerText;
+    });
+    if (status.search("未提交" < 0)) {
+      resubmit = true;
+      console.log(`Found status ${status}, going to resubmit.`)
+    }
+    if (is_same_day(latest_upload) && status.search("未提交") < 0) {
       await browser.close();
       console.log("Signed.")
     }
     else {
       // await page.screenshot({ path: 'tofill.png' })
-      // await page.evaluateOnNewDocument(function() {
+      const context = browser.defaultBrowserContext()
+      await context.overridePermissions("https://xg.hit.edu.cn", ['geolocation'])
+      await page.setGeolocation({
+        longitude: a.long + parseFloat(Number(Math.random() * 0.00006).toFixed(6)),
+        latitude: a.lat + parseFloat(Number(Math.random() * 0.00006).toFixed(6)),
+        accuracy: 65
+      })
+      // await page.evaluateOnNewDocument(function () {
       //   navigator.geolocation.getCurrentPosition = function (cb) {
       //     setTimeout(() => {
       //       cb({
@@ -89,20 +120,32 @@ const passwords = [
       //           altitude: null,
       //           altitudeAccuracy: null,
       //           heading: null,
-      //           latitude: a.lat+parseFloat(Number(Math.random()*0.00006).toFixed(6)),
-      //           longitude: a.long+parseFloat(Number(Math.random()*0.00006).toFixed(6)),
+      //           latitude: a.lat + parseFloat(Number(Math.random() * 0.00006).toFixed(6)),
+      //           longitude: a.long + parseFloat(Number(Math.random() * 0.00006).toFixed(6)),
       //           speed: null
       //         }
       //       })
-      //     }, Math.round(Math.random()*500+500))
+      //     }, Math.round(Math.random() * 500 + 500))
       //   }
       // });
-      await page.click('body > div.content > div.content_nr > div:nth-child(1) > a > div')
-      await page.waitForNavigation();
-      await page.evaluate(() => { document.getElementById("txfscheckbox").checked = true; document.getElementById("tw").value = 36; document.getElementById("tw1").value = 2 + Math.round(Math.random() * 3) });
-      await page.evaluate(() => { document.getElementById("txfscheckbox").checked = true; $("#gnxxdz").val(current_pos) });
+      console.log(`Resubmit is ${resubmit}`)
+      if (resubmit) {
+        await page.click('#center > div:nth-child(2)');
+      } else {
+        await page.click('body > div.content > div.content_nr > div:nth-child(1) > a > div')
+      }
+      try {
+        await page.waitForNavigation();
+      } catch (TimeoutException) {
+        console.warn("Timeout when reedirect to chart fill page. Maybe should be noticed while debugging.")
+      }
+      current_pos = a.current_pos;
+      // await page.evaluate(() => { document.getElementById("txfscheckbox").checked = true; document.getElementById("tw").value = 36; document.getElementById("tw1").value = 2 + Math.round(Math.random() * 3) });
+      await page.evaluate((current_pos) => { $("input#checkbox")[0].checked = true; $("#gnxxdz").val(current_pos) }, current_pos);
       await page.click('body > div.right_btn');
-      await page.waitForNavigation();
+      await page.waitForXPath('//*[@id="nrundefined"]/div[2]');
+      await page.click("#nrundefined > div.weui-dialog__ft > a.weui-dialog__btn.primary");
+      await page.waitFor(10000);
       console.log("Signed for 每日上报.");
 
       // 上报上午体温
